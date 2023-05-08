06206b8a3a4f27c31b245033926c856408f756bb
Rakesh <112846724+sandopu@users.noreply.github.com>
2023-05-08 22:56:18 +0530

    Add files via upload

---
+++ 13Q.R
@@ -0,0 +1,5 @@
+my_array <- array(data = c(1, 2, 3, 4, 5, 6), dim = c(2, 3))
+array1 <- array(data = c(1, 2, 3, 4, 5, 6), dim = c(2, 3))
+array2 <- array(data = c(6, 5, 4, 3, 2, 1), dim = c(2, 3))
+
+array_sum <- array1 + array2
