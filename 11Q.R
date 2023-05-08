228965f94a5de892db17c71275fab2968a9c621c
Rakesh <112846724+sandopu@users.noreply.github.com>
2023-05-08 22:55:44 +0530

    Add files via upload

---
+++ 11Q.R
@@ -0,0 +1,7 @@
+vec <- c(3, 1, 4, 1, 5, 9, 2, 6)
+
+min_val <- min(vec)
+cat("Minimum value in the vector is:", min_val, "\n")
+
+max_val <- max(vec)
+cat("Maximum value in the vector is:", max_val)
